757c1884540db781593190dadd97e6c455aece7f
Youness Chergui Amin <amirafes25@gmail.com>
2026-07-30 21:49:17 +0100

    Clean up Code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.button7.TabIndex = 8;
             this.button7.Tag = "?";
             this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
@@ -76,7 +76,7 @@
             this.button8.TabIndex = 7;
             this.button8.Tag = "?";
             this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
@@ -92,7 +92,7 @@
             this.button9.TabIndex = 6;
             this.button9.Tag = "?";
             this.button9.UseVisualStyleBackColor = false;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
@@ -108,7 +108,7 @@
             this.button4.TabIndex = 5;
             this.button4.Tag = "?";
             this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
@@ -124,7 +124,7 @@
             this.button5.TabIndex = 4;
             this.button5.Tag = "?";
             this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
@@ -140,7 +140,7 @@
             this.button6.TabIndex = 3;
             this.button6.Tag = "?";
             this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
@@ -156,7 +156,7 @@
             this.button3.TabIndex = 2;
             this.button3.Tag = "?";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
@@ -172,7 +172,7 @@
             this.button2.TabIndex = 1;
             this.button2.Tag = "?";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button1
             // 
@@ -189,7 +189,7 @@
             this.button1.TabIndex = 0;
             this.button1.Tag = "?";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // label2
             // 
@@ -303,6 +303,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "XODuel";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.button_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -203,51 +203,12 @@ namespace XO_Game_Final
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void button_Click(object sender, EventArgs e)
         {
-            ChangeImage(button1);
+            ChangeImage((Button) sender);
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button2);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button3);
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button6);
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button5);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button4);
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button9);
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button8);
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button7);
-        }
+       
 
         private void Form1_Load(object sender, EventArgs e)
         {
